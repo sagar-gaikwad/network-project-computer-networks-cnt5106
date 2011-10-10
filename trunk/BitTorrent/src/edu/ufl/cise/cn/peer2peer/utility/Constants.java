ee133cde13161e9d5fa02d3b4920d1ad0b248480
sagarandgaikwad@gmail.com <sagarandgaikwad@gmail.com@17e7a3c3-0ad2-9268-2451-9f89142188f1>
2011-10-10 23:45:44 +0000

    Changed type of message to BYTE.

--- trunk/BitTorrent/src/edu/ufl/cise/cn/peer2peer/utility/Constants.java
+++ trunk/BitTorrent/src/edu/ufl/cise/cn/peer2peer/utility/Constants.java
@@ -6,17 +6,17 @@ public class Constants {
 	public static final int RAW_DATA_SIZE = 1000;
 	public static final String XYZ = "xyz";
 	
-	public static final int HANDSHAKE_MESSAGE = 1;
-	public static final int BITFIEELD_MESSAGE = 2;
-	public static final int REQUEST_MESSAGE = 3;
-	public static final int PIECE_MESSAGE = 4;
-	public static final int INTERESTED_MESSAGE = 5;
-	public static final int NOT_INTERESTED_MESSAGE = 6;
-	public static final int HAVE_MESSAGE = 7;
+	public static final byte HANDSHAKE_MESSAGE = 1;
+	public static final byte BITFIEELD_MESSAGE = 2;
+	public static final byte REQUEST_MESSAGE = 3;
+	public static final byte PIECE_MESSAGE = 4;
+	public static final byte INTERESTED_MESSAGE = 5;
+	public static final byte NOT_INTERESTED_MESSAGE = 6;
+	public static final byte HAVE_MESSAGE = 7;
 
 	//changes by rhishikesh 
-	public static final int CHOKE_MESSAGE_CON = 0;
-	public static final int SIZE_OF_MESSAGE = 1;
+	public static final byte CHOKE_MESSAGE_CON = 0;
+	public static final int SIZE_OF_EMPTY_MESSAGE = 1;
 	
 	/** The Constant LOG_FILE_NAME. */
 	public static final String LOG_FILE_NAME = "logger.filename";
@@ -25,4 +25,7 @@ public class Constants {
 	public static final String LOGGER_NAME = "logger.name";
 	
 	public static final String CONFIGURATION_FILE = "common.cfg";
+	
+
+	public static final String PEER_INFO_FILE = "peerInfo.cfg";
 }

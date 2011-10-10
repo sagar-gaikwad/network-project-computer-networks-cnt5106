788b184fe7cef61c4e61a5ad2722a4c386ecc1fa
ag.mohit01 <ag.mohit01@gmail.com>
2011-10-10 19:00:34 +0000

    Mohits part updated. Yet to test write number of pieces part.

--- BitTorrent/src/edu/ufl/cise/cn/peer2peer/filehandler/BitFieldHandler.java
+++ BitTorrent/src/edu/ufl/cise/cn/peer2peer/filehandler/BitFieldHandler.java
@@ -2,8 +2,25 @@ package edu.ufl.cise.cn.peer2peer.filehandler;
 
 public class BitFieldHandler {
 	
+	//no size declaration of this array????
 	private boolean bitfieldVector[];
+	private int size;
+	public BitFieldHandler(int numOfPieces) {
+		bitfieldVector = new boolean[numOfPieces];
+		size = numOfPieces;
+		// TODO Auto-generated constructor stub
+		for(int i = 0; i < size; i++)
+		{
+			bitfieldVector[i] = false;
+		}
+		printvector();
+		
+	}
 
+	public int getLength()
+	{
+		return size;
+	}
 	public boolean[] getBitfieldVector() {
 		return bitfieldVector;
 	}
@@ -12,4 +29,23 @@ public class BitFieldHandler {
 		this.bitfieldVector = bitfieldVector;
 	}
 	
+	public boolean getBitFieldOn(int number)
+	{
+		return bitfieldVector[number];
+	}
+	public void setBitFieldOn(int number, boolean value)
+	{
+		bitfieldVector[number] = value;
+	}
+	
+	public void printvector()
+	{
+		int i =0;
+		System.out.println(" printing bitvector");
+		while (i < size)
+		{
+			System.out.print(" "+i+" : "+bitfieldVector[i++]);
+		}
+		System.out.println("");
+	}
 }

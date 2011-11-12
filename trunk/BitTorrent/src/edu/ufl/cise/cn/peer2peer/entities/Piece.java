be0a5cd447b90570a8882b7141ddae4b716c2a20
ag.mohit01@gmail.com <ag.mohit01@gmail.com@17e7a3c3-0ad2-9268-2451-9f89142188f1>
2011-11-12 16:17:13 +0000

    Updated datastrucutre of piece

--- trunk/BitTorrent/src/edu/ufl/cise/cn/peer2peer/entities/Piece.java
+++ trunk/BitTorrent/src/edu/ufl/cise/cn/peer2peer/entities/Piece.java
@@ -2,12 +2,21 @@ package edu.ufl.cise.cn.peer2peer.entities;
 
 public class Piece {
 	private byte[] data;
+	int size;
+	
+	public Piece(int size)
+	{
+		this.size = size;
+	}
 
 	public byte[] getData() {
 		return data;
 	}
 
 	public void setData(byte[] data) {
-		this.data = data;
+		if(this.size == data.length)
+			this.data = data;
+		else
+			System.err.println("Piece Size and Data Size MisMatch");
 	}
 }

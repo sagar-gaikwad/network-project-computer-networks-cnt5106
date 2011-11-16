8ffa9c8c2e165caa77f3a48d561b3829e0282e4a
ag.mohit01@gmail.com <ag.mohit01@gmail.com@17e7a3c3-0ad2-9268-2451-9f89142188f1>
2011-11-16 01:34:24 +0000

    Working giving number of preferred neighbours according to download speed

--- trunk/BitTorrent/src/edu/ufl/cise/cn/peer2peer/ChokeUnchokeManager.java
+++ trunk/BitTorrent/src/edu/ufl/cise/cn/peer2peer/ChokeUnchokeManager.java
@@ -60,37 +60,41 @@ public class ChokeUnchokeManager implements Runnable {
 	
 		// TODO Auto-generated method stub
 		Integer preferredNeighbors = 0;
-		HashMap<String,Float> speedMap = controller.getSpeedForAllPeers();
+		HashMap<String, Double> speedMap = controller.getSpeedForAllPeers();
 		// -- test code logging
-		System.out.println("ChokeUnchokeManager : run function called...");
+		/*System.out.println("ChokeUnchokeManager : run function called...");
 		System.out.println("printing hashmap of peerrs and download speed");
-		for(Entry<String, Float> entry : speedMap.entrySet()) 
+		for(Entry<String, Double> entry : speedMap.entrySet()) 
 		{
 			  String key = entry.getKey();
-			  Float value = entry.getValue();	
+			  Double value = entry.getValue();	
 			  System.out.println(" PeerID--> "+key+" Download Speed"+value);
 			  
-		}
+		}*/
 		// --- test code ends
-		/*
+		
 		if(PropsReader.getPropertyValue("NumberOfPreferredNeighbors") != null)
 			preferredNeighbors = Integer.parseInt(PropsReader.getPropertyValue("NumberOfPreferredNeighbors"));
 		else
 			System.err.println("NumberOfPreferredNeighbors variable not in properties file. Invalid Properties File!!!");
-		*/
 		preferredNeighbors = 3;
 		ArrayList<String> unchokePeers = new ArrayList();
 		//Find top k preferred neighbours
+		
 		//creating a treemap sorted on values for selecting top k preferred neighbors
-		TreeMap<String, Float> sortedSpeedMap = getSortedMap(speedMap);
+		System.out.println("SpeedMap Size"+speedMap.size());
+		ValueComparator bvc = new ValueComparator(speedMap);
+		TreeMap<String, Double> sortedSpeedMap = new TreeMap(bvc);		
+		sortedSpeedMap.putAll(speedMap);		
 		System.out.println("sorted map made");
+		
 		int count = 0;
 		//adding preferredNeighbors string to ArrayList
 		System.out.println("ChokeUnchokeManager : Sorting by download speed done");
-		for(Entry<String, Float> entry : sortedSpeedMap.entrySet()) 
+		for(Entry<String, Double> entry : sortedSpeedMap.entrySet()) 
 		{
 			  String key = entry.getKey();
-			  Float value = entry.getValue();
+			  Double value = entry.getValue();
 			  System.out.println("SortedPeer -->"+key+ " dload speed"+value);
 			  unchokePeers.add(key);
 			  count++; // maintaining count to break out of map iterator
@@ -100,14 +104,7 @@ public class ChokeUnchokeManager implements Runnable {
 		//unchoking those peers.
 		controller.chokePeers(unchokePeers);
 	}
-	//helper function to get sortedmap made out of hashmap
-	public TreeMap getSortedMap (HashMap unsortedMap)
-	{
-		ValueComparator bvc = new ValueComparator(unsortedMap);
-		TreeMap<String, Float> sortedMap = new TreeMap(bvc); 
-		sortedMap.putAll(unsortedMap);
-		return sortedMap;
-	}
+
 	//delay iin seconds
 	public void start(int startDelay, int intervalDelay){
 		System.out.println("ChokeUnchokeManager : start called now wait for "+intervalDelay+" seconds....");

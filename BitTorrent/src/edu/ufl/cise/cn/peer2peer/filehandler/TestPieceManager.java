f52cb12c2a196e5035b17ddef8eb756503231dc5
ag.mohit01 <ag.mohit01@gmail.com>
2011-11-28 02:09:03 +0000

    test file for piece manager

---
+++ BitTorrent/src/edu/ufl/cise/cn/peer2peer/filehandler/TestPieceManager.java
@@ -0,0 +1,43 @@
+package edu.ufl.cise.cn.peer2peer.filehandler;
+
+import edu.ufl.cise.cn.peer2peer.entities.Piece;
+import edu.ufl.cise.cn.peer2peer.utility.PropsReader;
+
+public class TestPieceManager 
+{
+	public static void main(String args[])
+	{
+		/*PieceManager piecem = new PieceManager(true, "1001");
+		piecem.getBitFieldHandler().printvector();
+		
+		//PieceManager piecem1 = PieceManager.getPieceManagerInstance(false, "1002");
+		//piecem.init(true, "1001");
+//		PropsReader prop = new PropsReader();
+		Piece sample0 = new Piece(Integer.parseInt(PropsReader.getPropertyValue("PieceSize")));
+		sample0 = piecem.getPiece(0);
+		
+		
+		Piece sample1 = new Piece(Integer.parseInt(PropsReader.getPropertyValue("PieceSize")));
+		sample1 = piecem.getPiece(1);
+		
+		
+		Piece sample2 = new Piece(Integer.parseInt(PropsReader.getPropertyValue("PieceSize")));
+		sample2 = piecem.getPiece(2);
+		
+		
+		Piece sample3 = new Piece(Integer.parseInt(PropsReader.getPropertyValue("PieceSize")));
+		sample3 = piecem.getPiece(3);
+		
+		
+		Piece sample4 = new Piece(Integer.parseInt(PropsReader.getPropertyValue("PieceSize")));
+		sample4 = piecem.getPiece(4);
+		PieceManager piecem1 = new PieceManager(false, "1002");
+		piecem1.writePiece(0, sample0);
+		piecem1.writePiece(1, sample1);
+		piecem1.writePiece(2, sample2);
+		piecem1.writePiece(3, sample3);
+		piecem1.writePiece(4, sample4);
+*/	}
+	
+
+}
